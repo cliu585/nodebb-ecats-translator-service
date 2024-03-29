be1b67af7959e13dc8cc1f38b16bea7894ca2799
EmilyMSong <emilyminasong@gmail.com>
2024-03-29 12:55:12 -0400

    add test cases

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -7,7 +7,31 @@ def test_chinese():
     assert translated_content == "This is a Chinese message"
 
 def test_llm_normal_response():
-    pass
+    is_english, translated_content = translate_content("This is an English message")
+    assert is_english == True
+    assert translated_content == "This is an English message"
 
 def test_llm_gibberish_response():
-    pass
+    is_english, translated_content = translate_content("askdfalkjwejf klwqj")
+    assert is_english == True
+    assert translated_content == "askdfalkjwejf klwqj"
+
+# @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+# def test_unexpected_language(mocker):
+#   mocker.return_value.text = "I don't understand your request"
+#   assert translate_content("Aquí está su primer ejemplo.") == (True, "Aquí está su primer ejemplo.")
+
+# @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+# def test_return_none(mocker):
+#   mocker.return_value.text = None
+#   assert query_llm_robust("Aquí está su primer ejemplo.") == (True, "Aquí está su primer ejemplo.")
+
+# @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+# def test_return_nonstring(mocker):
+#   mocker.return_value.text = []
+#   assert query_llm_robust("Aquí está su primer ejemplo.") == (True, "Aquí está su primer ejemplo.")
+
+# @patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+# def test_return_empty(mocker):
+#   mocker.return_value.text = ""
+#   assert query_llm_robust("Aquí está su primer ejemplo.") == (True, "Aquí está su primer ejemplo.")

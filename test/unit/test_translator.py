f8764169e68a009709329aed7a0f2b0ceb66ea0b
p0nk0 <Roberts.taylor000@gmail.com>
2024-04-04 23:52:51 -0400

    added missed period in test cases

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -6,7 +6,7 @@ import ipytest
 def test_chinese():
     is_english, translated_content = translate_content("这是一条中文消息.")
     assert is_english == False
-    assert translated_content == "This is a Chinese message"
+    assert translated_content == "This is a Chinese message."
 
 def test_llm_normal_response():
     is_english, translated_content = translate_content("This is an English message.")
